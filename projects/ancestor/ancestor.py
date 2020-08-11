fb004ce1f6c58fc86271b001b81705862a0fc3c8
dawsonhammdev <hammsdesigns@gmail.com>
2020-08-11 18:01:10 -0500

    working on the ancestors.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,11 @@
+from graph import Graph
+
 
 def earliest_ancestor(ancestors, starting_node):
+    # we need to use a BFT to traverse through the family tree and find the 'oldest' node in the tree with paths to the starting node.
+    # the oldest node will be where there are no parents.
+    # if there are two parents then take the lowest value assigned.
+    # if statrting node has no ancestors return -1
+    if starting_node
+
     pass
